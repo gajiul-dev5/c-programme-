1c3b6ea345c4c0bc351a569dc9b3ee21f4de6cea
gajiul-dev5 <mdgajiul05@gmail.com>
2024-11-03 20:21:02 +0600

    Add files via upload

---
+++ datatype1.c
@@ -0,0 +1,23 @@
+/*
+An integer variable number that will contain 10,
+write a program that prints an integer number
+
+*/
+#include<stdio.h>
+int main()
+{
+    int num1=10,num2=20;
+
+    printf("Number are %d,%d\n",num1,num2);
+
+
+
+
+
+
+
+
+
+
+
+}

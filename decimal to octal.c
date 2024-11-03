dc8e59ad8f8f4d3f656c263faab2c158ee4e7f42
gajiul-dev5 <mdgajiul05@gmail.com>
2024-11-03 20:31:12 +0600

    Add files via upload

---
+++ decimal to octal.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+     int number;
+     printf("Enter a decimal number:");
+
+     scanf("%d",&number);
+     printf("Octal number= %o",number);
+
+
+     getch();
+
+
+
+
+
+
+}

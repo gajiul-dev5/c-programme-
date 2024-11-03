517b2595711350b19200f1643dbcf52e827457de
gajiul-dev5 <mdgajiul05@gmail.com>
2024-11-03 20:30:20 +0600

    Add files via upload

---
+++ lowercase to uppercase.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    char lower;
+    printf("Enter any lowercase :");//a=97
+    scanf("%c",&lower);
+
+
+    printf("The uppercase is : %c \n",lower-32);//A=65
+
+
+
+
+
+
+
+
+}

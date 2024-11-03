e34be03f318d903ae414f6054882a838684f49ae
gajiul-dev5 <mdgajiul05@gmail.com>
2024-11-03 20:25:09 +0600

    Add files via upload

---
+++ gets,fgets,getchar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    char name[50];
+
+    printf("Enter your name = ");
+
+    fgets(name,sizeof(name),stdin);
+    printf("hello ");
+    puts(name);
+
+
+    getch();
+
+
+
+
+}

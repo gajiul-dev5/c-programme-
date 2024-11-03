efe2fc99754f0e3f8e2ac475acb97cbf231c3969
gajiul-dev5 <mdgajiul05@gmail.com>
2024-11-03 20:27:04 +0600

    Add files via upload

---
+++ get input.c
@@ -0,0 +1,19 @@
+/*
+write a program that takes an integer
+ and prints that number
+*/
+#include<stdio.h>
+int main()
+{
+
+    int num;
+    printf("enter a integer number : ");
+    scanf("%d",&num);
+    printf("you have pressed :%d\n",num);
+
+
+    return 0;
+
+
+
+}

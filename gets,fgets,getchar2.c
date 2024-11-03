b74acd250fd9dab5b44b67341c20ddf7ecbad811
gajiul-dev5 <mdgajiul05@gmail.com>
2024-11-03 20:25:23 +0600

    Add files via upload

---
+++ gets,fgets,getchar2.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int main()
+{
+    char character;
+    printf("Enter any character=");
+    character=getchar();
+    printf("You entered : %c\n",character);
+
+    return 0;
+
+
+
+
+
+
+
+}

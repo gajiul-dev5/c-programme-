85bfc9f5893ee714453c1c603899c32ff1a99fed
gajiul-dev5 <mdgajiul05@gmail.com>
2024-11-03 20:28:58 +0600

    Add files via upload

---
+++ convert ascci value to char.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+    int n;
+    printf("Enter any Ascii value : ");
+    scanf("%d",&n);
+
+
+    printf("The Ascii  character is : %c\n",n);
+    return 0;
+
+
+
+}

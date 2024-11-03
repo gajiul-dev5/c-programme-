d2f79ab03ce6b9e2bffd03ebb7238d1758ce275f
gajiul-dev5 <mdgajiul05@gmail.com>
2024-11-03 20:24:02 +0600

    Add files via upload

---
+++ scanf.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+
+    int num1;
+    float num2 ;
+
+    printf("Enter A Integer  & A Floating Number = ");
+    scanf("%d %f",&num1,&num2);
+
+
+    printf("The Numbers = %d %.1f\n",num1,num2);
+
+
+    return 0;
+
+
+
+
+
+}

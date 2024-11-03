182097535fcddb8c8c70ed1435454ff58e702f67
gajiul-dev5 <mdgajiul05@gmail.com>
2024-11-03 20:22:46 +0600

    Add files via upload

---
+++ new line.c
@@ -0,0 +1,20 @@
+
+/*This is the programe which is show us the name of
+owner
+his univarsity name
+also his phone number
+
+
+*/
+#include<stdio.h>
+int main()
+{
+
+
+    printf("Md. Gajiul islam\n");
+      printf("daffodil international univarsity\n");
+        printf("01887697423\n");
+
+
+        getch();
+}

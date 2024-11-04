e23b34aa4b7100eb462426a29b977377356cf4c3
gajiul-dev5 <mdgajiul05@gmail.com>
2024-11-04 19:02:48 +0600

    Add files via upload

---
+++ cls 30 octal to hexa decimal.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("Enter a  octal number=");
+    scanf("%o",&number);
+    printf("Hexa-decimal number=%x",number);
+    getch();
+
+
+
+
+
+}
